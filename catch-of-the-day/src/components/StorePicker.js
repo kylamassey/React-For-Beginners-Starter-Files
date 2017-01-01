d84e52cbae91751185e99c0d99b5a79a7a635ac8
Kyla Massey <sir.massey@gmail.com>
2016-12-31 19:30:49 -0500

    video 11 react events completed

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,22 +2,30 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
-  gotToStore() {
+  // constuctor() {
+  //   super();
+  //   this.gotToStore = this.gotToStore.bind(this);
+  // }
+  gotToStore(event) {
+    event.preventDefault();
     console.log('You Changed the URL');
     // first grab text from box
+    console.log(this.storeInput.value);
     // second transition from / to / store/:storeId
   }
   render() {
     /* cannot put comment at the top level of what is returned */
     return (
-      <form className="store-selector" onSubmit={this.gotToStore}>
+      <form className="store-selector" onSubmit={(e) => this.gotToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
-          defaultValue={getFunName()} />
+          defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
   }
 }
+
+
 
 export default StorePicker;
