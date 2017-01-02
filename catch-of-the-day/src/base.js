f57edf863dbf41fa840dd67576c811094dc9bae8
Kyla Massey <sir.massey@gmail.com>
2017-01-01 19:20:49 -0500

    firebase database sync and state manipulation

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBQN7iBIllaVX72yJYa7c8CcamN3-3qeQw",
+    authDomain: "catch-of-the-day-kyla-massey.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-kyla-massey.firebaseio.com",
+});
+
+export default base;
